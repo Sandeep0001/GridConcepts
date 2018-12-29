8be183de14c92d1a9fceb47e1d896e3b81e20f27
Sandeep I S <is.sandeep7@gmail.com>
2018-12-29 14:24:55 +0530

    Grid Concepts

---
+++ src/main/java/GridTest/ChromeGridTest.java
@@ -0,0 +1,34 @@
+package GridTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class ChromeGridTest {
+
+	public static void main(String[] args) throws MalformedURLException {
+
+		//1. define desired capabilities:
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setBrowserName("chrome");
+		cap.setPlatform(Platform.WIN8_1);
+		
+		//2. Chrome options definition:
+		ChromeOptions options = new ChromeOptions();
+		options.merge(cap);
+		
+		String hubUrl = "http://192.168.0.3:4444/wd/hub";
+		WebDriver driver = new RemoteWebDriver(new URL(hubUrl), options);
+		
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		
+		driver.quit();
+	}
+
+}
